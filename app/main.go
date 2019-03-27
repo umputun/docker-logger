9c8f1d543534b041a618c67a8707c99f6f761d82
Vasilii Blazhnov <v@blazhnov.me>
2019-03-27 23:27:40 +0500

    disable syslog and fix compile at windows

--- app/main.go
+++ app/main.go
@@ -4,15 +4,14 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"log/syslog"
 	"os"
 	"os/signal"
 	"syscall"
 
 	dockerclient "github.com/fsouza/go-dockerclient"
 	log "github.com/go-pkgz/lgr"
-	flags "github.com/jessevdk/go-flags"
-	lumberjack "gopkg.in/natefinch/lumberjack.v2"
+	"github.com/jessevdk/go-flags"
+	"gopkg.in/natefinch/lumberjack.v2"
 
 	"github.com/umputun/docker-logger/app/discovery"
 	"github.com/umputun/docker-logger/app/logger"
@@ -45,6 +44,10 @@ func main() {
 
 	if opts.Includes != nil && opts.Excludes != nil {
 		log.Fatalf("[ERROR] only single option Excludes/Includes are allowed")
+	}
+
+	if opts.EnableSyslog && !isSyslogSupported() {
+		log.Fatalf("[ERROR] syslog is not supported on current OS")
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -167,7 +170,7 @@ func makeLogWriters(containerName string, group string, isExt bool) (logWriter, 
 	}
 
 	if opts.EnableSyslog {
-		syslogWriter, err := syslog.Dial("udp4", opts.SyslogHost, syslog.LOG_WARNING|syslog.LOG_DAEMON, "docker/"+containerName)
+		syslogWriter, err := getSyslogWriter(opts.SyslogHost, containerName)
 
 		if err == nil {
 			logWriters = append(logWriters, syslogWriter)
