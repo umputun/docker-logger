6a577acba8abeff971f22828e68bd313e846c9bc
Umputun <umputun@gmail.com>
2019-03-27 22:28:52 -0500

    add test for json writer and syslog

--- app/main_test.go
+++ app/main_test.go
@@ -12,6 +12,7 @@ func Test_makeLogWriters(t *testing.T) {
 	defer os.RemoveAll("/tmp/logger.test")
 	opts := cliOpts{FilesLocation: "/tmp/logger.test", EnableFiles: true, MaxFileSize: 1, MaxFilesCount: 10}
 	stdWr, errWr := makeLogWriters(opts, "container1", "gr1")
+	assert.NotEqual(t, stdWr, errWr, "different writers for out and err")
 
 	// write to out writer
 	_, err := stdWr.Write([]byte("abc line 1\n"))
@@ -41,6 +42,7 @@ func Test_makeLogWritersMixed(t *testing.T) {
 	defer os.RemoveAll("/tmp/logger.test")
 	opts := cliOpts{FilesLocation: "/tmp/logger.test", EnableFiles: true, MaxFileSize: 1, MaxFilesCount: 10, MixErr: true}
 	stdWr, errWr := makeLogWriters(opts, "container1", "gr1")
+	assert.Equal(t, stdWr, errWr, "same writer for out and err in mixed mode")
 
 	// write to out writer
 	_, err := stdWr.Write([]byte("abc line 1\n"))
@@ -60,4 +62,41 @@ func Test_makeLogWritersMixed(t *testing.T) {
 
 	assert.NoError(t, stdWr.Close())
 	assert.NoError(t, errWr.Close())
+}
+
+func Test_makeLogWritersWithJSON(t *testing.T) {
+	defer os.RemoveAll("/tmp/logger.test")
+	opts := cliOpts{FilesLocation: "/tmp/logger.test", EnableFiles: true, MaxFileSize: 1, MaxFilesCount: 10, ExtJSON: true}
+	stdWr, errWr := makeLogWriters(opts, "container1", "gr1")
+
+	// write to out writer
+	_, err := stdWr.Write([]byte("abc line 1"))
+	assert.NoError(t, err)
+
+	r, err := ioutil.ReadFile("/tmp/logger.test/gr1/container1.log")
+	assert.NoError(t, err)
+	assert.Contains(t, string(r), `"msg":"abc line 1","container":"container1","group":"gr1"`)
+
+	_, err = os.Stat("/tmp/logger.test/gr1/container1.err")
+	assert.NotNil(t, err)
+
+	assert.NoError(t, stdWr.Close())
+	assert.NoError(t, errWr.Close())
+}
+
+func Test_makeLogWritersSyslogFailed(t *testing.T) {
+	opts := cliOpts{EnableSyslog: true}
+	stdWr, errWr := makeLogWriters(opts, "container1", "gr1")
+	assert.Equal(t, stdWr, errWr, "same writer for out and err in syslog")
+	// write to out writer
+	_, err := stdWr.Write([]byte("abc line 1\n"))
+	assert.NoError(t, err)
+	_, err = stdWr.Write([]byte("xxx123 line 2\n"))
+	assert.NoError(t, err)
+
+	// write to err writer
+	_, err = errWr.Write([]byte("err line 1\n"))
+	assert.NoError(t, err)
+	_, err = errWr.Write([]byte("xxx123 line 2\n"))
+	assert.NoError(t, err)
 }
